8a33bc8506526402f063c61d32671085d1f7a1cd
Less <31947204+lessandsass@users.noreply.github.com>
2020-12-22 17:55:20 +0530

    2.API Usage & HTTP Client - Part 2 is on 14:40

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'index');
-Route::view('/movie', 'show');
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
+Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.movie');
 
- 
+
